9c244b009c7d9e9a456ec06969f5e483c8baedd7
Austin Macdonald <austin@redhat.com>
2022-12-16 15:25:38 -0500

    Initial implementation of matrix room cron-logger

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+
+def load_config_from_env():
+   # TODO(validate) all necessary confs have value
+   required_conf = {
+      "user_id": "MATRIX_USERNAME",
+      "device_id": "MATRIX_DEVICE_ID",
+      "password": "MATRIX_PASSWORD",
+      "homeserver": "MATRIX_HOST",
+      "room_id": "MATRIX_ROOM_ID",
+      "store_path": "MATRIX_STORE_PATH",
+   }
+
+   config = {}
+   for key, env_var in required_conf.items():
+      value = os.environ.get(env_var)
+      config[key] = value
+
+   return config
